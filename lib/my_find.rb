307b2e9ba788be71c551729db638e1bb4b46cb30
Richard Fenix <richard.fenix@gmail.com>
2018-05-30 04:04:24 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,19 @@
 require 'pry'
 
 def my_find(collection)
+  i = 0 
+  block_return_values = []
+  while i < collection.length 
+    block_return_values << yield (collection[i])
+    i += 1
+  end
+  
+  if block_return_values.include(false)
+    false
+  else
+    true 
+  end
 
 end
+
+my_find([1..100]) {|i| i % 3 ==0 && i % 5 == 0}
